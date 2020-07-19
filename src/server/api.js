1a65dfad90a945db1e309fe4fb524bc59b46650f
Maddie Lord <madeline.lord@4cinsights.com>
2020-07-18 17:16:58 -0700

    config changes for heroku

--- src/server/api.js
+++ src/server/api.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import db from './db.js';
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8080;
 const app = express();
 
 app.get('/board/:boardId', async(req, res) => {
