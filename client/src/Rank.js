41d8c53a64345066af43d351bcb1c0a486c2d6b1
Maddie Lord <madeline.lord@4cinsights.com>
2020-07-19 09:57:13 -0700

    project restructure to serve client from server for heroku deployment

--- client/src/Rank.js
+++ client/src/Rank.js
@@ -9,14 +9,14 @@ class Rank extends React.Component {
 
     async componentDidMount() {
         try {
-            const issuesResponse = await fetch("/board/1");
+            const issuesResponse = await fetch("/api/board/1");
             const issues = await issuesResponse.json();
 
             // todo: change endpoint to accept list of users and only grab users on board
-            const usersResponse = await fetch("/users");
+            const usersResponse = await fetch("/api/users");
             const users = await usersResponse.json();
 
-            const resolutionValuesResponse = await fetch("/resolutionValues");
+            const resolutionValuesResponse = await fetch("/api/resolutionValues");
             const resolutionValues = await resolutionValuesResponse.json();
 
             this.setState({ 
---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import path from 'path';
+import db from './db.js';
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(path.resolve(path.dirname(''), './client/build')));
+
+app.get('/api/board/:boardId', async(req, res) => {
+    res.set('Content-Type', 'application/json');
+    return res.send(await db.getIssuesForBoard(req.params.boardId));
+})
+
+app.get('/api/users', async (req, res) => {
+    res.set('Content-Type', 'application/json');
+    return res.send(await db.getUsers());
+})
+
+app.get('/api/resolutionValues', async (req, res) => {
+    res.set('Content-Type', 'application/json');
+    return res.send(await db.getResolutionValues());
+})
+
+app.get('*', function (req, res) {
+    res.sendFile(path.resolve(path.dirname(''), './client/build', 'index.html'));
+});
+
+app.listen(port, () =>
+    console.log(`listening on port ${port}`),
+);
